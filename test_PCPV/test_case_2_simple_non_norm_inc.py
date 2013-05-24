251e443639be66f5620080939c7dc7a689ed55dd
Bjorn Sturmberg <bjorn@physics.usyd.edu.au>
2013-05-24 13:40:16 +1000

    further clean up of Data folder and test reference data updated to redefined test ordering and refined mesh for TW.

--- test_PCPV/test_case_2_simple_non_norm_inc.py
+++ test_PCPV/test_case_2_simple_non_norm_inc.py
@@ -77,11 +77,11 @@ def setup_module(module):
     t_r_a_plots(stack_list)
 
 
-    # SAVE DATA AS REFERENCE
-    # Only run this after changing what is simulated - this
-    # generates a new set of reference answers to check against
-    # in the future
-    testing.save_reference_data("case_2", stack_list)
+    # # SAVE DATA AS REFERENCE
+    # # Only run this after changing what is simulated - this
+    # # generates a new set of reference answers to check against
+    # # in the future
+    # testing.save_reference_data("case_2", stack_list)
 
 def results_match_reference(filename):
     reference = np.loadtxt("ref/case_3/" + filename)
