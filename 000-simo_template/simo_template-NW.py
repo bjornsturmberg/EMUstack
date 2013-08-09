db49865e0393edd33e7abec65c55f7eca7c0671a
Bjorn Sturmberg <bjorn@physics.usyd.edu.au>
2013-08-09 17:59:27 +1000

    Almost neglected to include template files.

---
+++ 000-simo_template/simo_template-NW.py
@@ -0,0 +1,235 @@
+"""
+The Ueber python script, the only one that needs to be edited to set up all 
+simulation parameters.
+Uses other python scripts to prime the simulation (interpolate raw data over chosen 
+wavelengths etc.), then calls the fortran routine pcpv.exe for each wavelength giving 
+it all the required details. It does this by spanning a new process for each wavelength,
+keeping the total running instances to a maximum number (num_cores_to_use). Finally all 
+results are collected in text files and the spectra are plotted. A log file is found in
+python_log.txt
+"""
+
+import time
+import datetime
+import numpy as np
+import sys
+from multiprocessing import Pool
+# import multiprocessing   as mp
+sys.path.append("../PCPV/")
+
+import clear_previous
+import objects
+import materials
+import plotting
+from stack import *
+
+
+# The following should be in the function "setup_module()",
+# but unfortunately simulate_stack is defined in a lazy-but-easy
+# way: the structures are inherited rather than passed in.
+
+start = time.time()
+################ Simulation parameters ################
+
+simo_para  = objects.Controls(debug = 0,max_order_PWs = 3, num_cores = 5,
+    PrintAll = 0, Checks = 0, PrintSolution = 0, PrintSupModes = 0)
+# Remove results of previous simulations
+clear_previous.clean('.txt')
+clear_previous.clean('.pdf')
+clear_previous.clean('.gif')
+clear_previous.clean('.log')
+
+################ Light parameters #####################
+wl_1     = 310
+wl_2     = 1127
+no_wl_1  = 3
+# Set up light objects
+wavelengths = np.linspace(wl_1, wl_2, no_wl_1)
+light_list  = [objects.Light(wl) for wl in wavelengths]
+# # Single wavelength run
+# wl_super = 450
+# wavelengths = np.array([wl_super])
+# light_list  = [objects.Light(wl) for wl in wavelengths]
+
+
+# period must be consistent throughout simulation!!!
+period = 600.0
+
+cover  = objects.ThinFilm(period = period, height_nm = 'semi_inf',
+    material = materials.Air, loss = True)
+
+bottom1  = objects.ThinFilm(period = period, height_nm = 'semi_inf',
+    material = materials.Air, loss = False)
+bottom2  = objects.ThinFilm(period = period, height_nm = 100,
+    material = materials.SiO2_a, loss = True)
+TF4  = objects.ThinFilm(period = period, height_nm = 200,
+    material = materials.Si_c, loss = True)
+bottom3  = objects.ThinFilm(period = period, height_nm = 'semi_inf',
+    material = materials.TiO2, loss = False)
+
+NWs = objects.NanoStruct('NW_array', period, 60, height_nm = 2330, 
+    inclusion_a = materials.Si_c, background = materials.Air, 
+    loss = True, nb_typ_el = 4, 
+    make_mesh_now = True, force_mesh = True, lc_bkg = 0.2, lc2= 1.0)#Material(1.0 + 0.0j)
+
+stack_list = []
+# Find num_BM for each simulation in a somewhat arbitrary way
+# Maybe roll this out into a Bjorn-specific function
+max_n = max([NWs.inclusion_a.n(wl).real for wl in wavelengths])
+max_num_BMs = 200
+
+def simulate_stack(light):
+    num_BM = round(max_num_BMs * NWs.inclusion_a.n(light.wl_nm).real/max_n)
+    # num_BM = max_num_BMs
+    
+    ################ Evaluate each layer individually ##############
+    sim_cover = cover.calc_modes(light, simo_para)
+    sim_bot1 = bottom1.calc_modes(light, simo_para)
+    sim_bot2 = bottom2.calc_modes(light, simo_para)
+    sim_bot3 = bottom3.calc_modes(light, simo_para)
+    sim_TF4  = TF4.calc_modes(light, simo_para)
+    # sim_NWs = NWs.calc_modes(light, simo_para, num_BM = num_BM)
+
+    ################ Evaluate full solar cell structure ##############
+    """ Now when defining full structure order is critical and
+    solar_cell list MUST be ordered from bottom to top!
+    """
+
+    stack1 = Stack((sim_bot1,sim_cover))
+    # stack2 = Stack((sim_bot1,sim_bot2,sim_NWs, sim_cover))
+    stack2 = Stack((sim_bot1,sim_bot2,sim_TF4, sim_cover))
+    stack1.calc_scat(pol = 'TE')
+    stack2.calc_scat(pol = 'TE')
+
+# multiple heights for sim_TF4
+    stack3_indiv_hs = []
+    average_t = 0
+    average_r = 0
+    average_a = 0
+
+    num_h = 10
+    for h in np.linspace(100,2000,num_h):
+        stack3 = Stack((sim_bot1,sim_bot2,sim_TF4, sim_cover), heights_nm = ([bottom2.height_nm,h]))
+        stack3.calc_scat(pol = 'TE')
+
+        stack3_indiv_hs.append(stack3)
+
+        average_t += stack3.t_list[-1]/num_h
+        average_r += stack3.r_list[-1]/num_h
+        average_a += stack3.a_list[-1]/num_h
+    stack3.t_list[-1] = average_t
+    stack3.r_list[-1] = average_r
+    stack3.a_list[-1] = average_a
+
+# stack3 contains info on the last height and the average spectra
+    return [stack1,stack2,stack3,stack3_indiv_hs] 
+    # return stack1
+
+
+# Run in parallel across wavelengths.
+pool = Pool(simo_para.num_cores)
+stacks_wl_list = pool.map(simulate_stack, light_list)
+# Run one at a time
+# stacks_wl_list = map(simulate_stack, light_list)
+
+
+
+# Pull apart simultaneously simulated stakes into single stack, all wls arrays.
+# Unnecissary if just returning a single stack
+np.array(stacks_wl_list)
+# stack1_wl_list = []
+# for i in range(len(wavelengths)):
+#     stack1_wl_list.append(stacks_wl_list[i][0])
+
+
+######################## Plotting ########################
+last_light_object = light_list.pop()
+param_layer = TF4 # Specify the layer for which the parameters should be printed on figures.
+params_string = plotting.gen_params_string(param_layer, last_light_object, simo_para.max_order_PWs,
+    max_num_BMs=max_num_BMs)
+
+
+
+#### Example 1: simple multilayered stack.
+stack_label = 1 # Specify which stack you are dealing with.
+stack1_wl_list = []
+for i in range(len(wavelengths)):
+    stack1_wl_list.append(stacks_wl_list[i][stack_label])
+active_layer_nu = 2 # Specify which layer is the active one (where absorption generates charge carriers).
+# Plot total transmission, reflection, absorption & that of each layer. 
+# Also calculate efficiency of active layer.
+Efficiency = plotting.t_r_a_plots(stack1_wl_list, wavelengths, params_string, 
+    active_layer_nu=active_layer_nu, stack_label=stack_label) 
+# Dispersion
+plotting.omega_plot(stack1_wl_list, wavelengths, params_string, stack_label=stack_label) 
+# # Energy Concentration
+# which_layer = 2
+# which_modes = [1,2] # can be a single mode or multiple
+# plotting.E_conc_plot(stack1_wl_list, which_layer, which_modes, wavelengths, 
+#     params_string, stack_label=stack_label)
+
+
+# #### Example 2: averaged multilayered stack where one layer has many heights.
+# stack_label = 2
+# active_layer_nu = 2
+# stack2_wl_list = []
+# for i in range(len(wavelengths)):
+#     stack2_wl_list.append(stacks_wl_list[i][stack_label])
+# Efficiency = plotting.t_r_a_plots(stack2_wl_list, wavelengths, params_string, 
+#     active_layer_nu=active_layer_nu, stack_label=stack_label)
+
+
+
+#### Example 3: individual spectra of multilayered stack where one layer has many heights.
+stack_label = 3
+active_layer_nu = 2
+number_of_hs = len(stacks_wl_list[0][stack_label])
+for h in range(number_of_hs):
+    gen_name = '_h-'
+    h_name = str(h)
+    additional_name = gen_name+h_name # You can add an arbitry string onto the end of the spectra filenames.
+    stack3_hs_wl_list = []
+    for i in range(len(wavelengths)):
+        stack3_hs_wl_list.append(stacks_wl_list[i][stack_label][h])
+    Efficiency = plotting.t_r_a_plots(stack3_hs_wl_list, wavelengths, params_string, 
+        active_layer_nu=active_layer_nu, stack_label=stack_label, add_name = additional_name)
+# Animate spectra as a function of heights.
+from os import system as ossys
+delay = 5 # delay between images in gif in seconds
+names = 'Lay_Absorb_stack'+str(stack_label)+gen_name
+gif_cmd = 'convert -delay %(d)i +dither -layers Optimize -colors 16 %(n)s*.pdf %(n)s.gif'% {
+'d' : delay, 'n' : names}
+ossys(gif_cmd)
+opt_cmd = 'gifsicle -O2 %(n)s.gif -o %(n)s-opt.gif'% {'n' : names}
+ossys(opt_cmd)
+rm_cmd = 'rm %(n)s.gif'% {'n' : names}
+ossys(rm_cmd)
+
+
+
+######################## Single Wavelength Plotting ########################
+# Plot transmission as a function of k vector.
+# plotting.t_func_k_plot(stack3_wl_list)
+
+# plot_scat_mats # done in stack.py
+
+
+######################## Wrapping up ########################
+print '\n*******************************************'
+print 'The ultimate efficiency is %12.8f' % Efficiency
+print '-------------------------------------------'
+
+# Calculate and record the (real) time taken for simulation
+elapsed = (time.time() - start)
+hms     = str(datetime.timedelta(seconds=elapsed))
+hms_string = 'Total time for simulation was \n \
+    %(hms)s (%(elapsed)12.3f seconds)'% {
+            'hms'       : hms,
+            'elapsed'   : elapsed, }
+
+python_log = open("python_log.log", "w")
+python_log.write(hms_string)
+python_log.close()
+
+print hms_string
+print '*******************************************'
---
+++ 000-simo_template/simo_template-grating.py
@@ -0,0 +1,160 @@
+"""
+The Ueber python script, the only one that needs to be edited to set up all 
+simulation parameters.
+Uses other python scripts to prime the simulation (interpolate raw data over chosen 
+wavelengths etc.), then calls the fortran routine pcpv.exe for each wavelength giving 
+it all the required details. It does this by spanning a new process for each wavelength,
+keeping the total running instances to a maximum number (num_cores_to_use). Finally all 
+results are collected in text files and the spectra are plotted. A log file is found in
+python_log.txt
+"""
+
+import time
+import datetime
+import numpy as np
+import sys
+from multiprocessing import Pool
+# import multiprocessing   as mp
+sys.path.append("../PCPV/")
+
+import clear_previous
+import objects
+import materials
+import plotting
+from stack import *
+
+
+# The following should be in the function "setup_module()",
+# but unfortunately simulate_stack is defined in a lazy-but-easy
+# way: the structures are inherited rather than passed in.
+
+################ Simulation parameters ################
+
+simo_para  = objects.Controls(debug = 0,max_order_PWs = 5, num_cores = 3,
+    PrintAll = 0, Checks = 0, PrintSolution = 0, PrintSupModes = 0)
+# Remove results of previous simulations
+clear_previous.clean('.txt')
+clear_previous.clean('.pdf')
+clear_previous.clean('.log')
+
+################ Light parameters #####################
+wl_1     = 900
+wl_2     = 1200
+no_wl_1  = 3
+# Set up light objects
+wavelengths = np.linspace(wl_1, wl_2, no_wl_1)
+light_list  = [objects.Light(wl) for wl in wavelengths]
+# # Single wavelength run
+# wl_super = 1000
+# wavelengths = np.array([wl_super])
+# light_list  = [objects.Light(wl) for wl in wavelengths]
+
+
+# period must be consistent throughout simulation!!!
+period = 120
+
+cover  = objects.ThinFilm(period = period, height_nm = 'semi_inf',
+    material = materials.Material(3.5 + 0.0j), loss = True)
+
+homo_film  = objects.ThinFilm(period = period, height_nm = 5,
+    material = materials.Material(3.6 + 0.27j), loss = True)
+
+bottom = objects.ThinFilm(period = period, height_nm = 'semi_inf',
+    material = materials.Air, loss = False)
+
+grating_1 = objects.NanoStruct('1D_grating', period, 100, height_nm = 25, 
+    inclusion_a = materials.Ag, background = materials.Material(1.5 + 0.0j), loss = True, nb_typ_el = 4, 
+    make_mesh_now = True, force_mesh = True, lc_bkg = 0.05, lc2= 4.0)
+
+mirror = objects.ThinFilm(period = period, height_nm = 100,
+    material = materials.Ag, loss = True)
+
+
+stack_list = []
+# Find num_BM for each simulation in a somewhat arbitrary way
+# Maybe roll this out into a Bjorn-specific function
+max_n = max([grating_1.inclusion_a.n(wl).real for wl in wavelengths])
+max_num_BMs = 163
+
+def simulate_stack(light):
+    num_BM = max_num_BMs#round(max_num_BMs * grating_1.inclusion_a.n(light.wl_nm).real/max_n)
+    
+    ################ Evaluate each layer individually ##############
+    sim_cover = cover.calc_modes(light, simo_para)
+    sim_homo_film = homo_film.calc_modes(light, simo_para)
+    sim_bot = bottom.calc_modes(light, simo_para)
+    sim_grat1 = grating_1.calc_modes(light, simo_para, num_BM = num_BM)
+    sim_mirror = mirror.calc_modes(light, simo_para)
+
+    ################ Evaluate full solar cell structure ##############
+    """ Now when defining full structure order is critical and
+    solar_cell list MUST be ordered from bottom to top!
+    """
+    stack = Stack((sim_bot, sim_mirror, sim_grat1, sim_homo_film, sim_cover))
+    stack.calc_scat(pol = 'TE')
+
+    return stack
+
+
+start = time.time()
+
+# Run in parallel across wavelengths.
+pool = Pool(simo_para.num_cores)
+stack_list = pool.map(simulate_stack, light_list)
+# # Run one at a time
+# module.stack_list = map(simulate_stack, light_list)
+    
+
+t_r_a_plots(stack_list)
+
+# specify which layer is the active one (where absorption generates charge carriers)
+active_layer = homo_film
+act_lay_nu   = 0
+# specify which layer for which the parameters should be printed on figures
+lay_print_params = grating_1
+
+
+
+################# Efficiency & weighted spectra for active layer ################
+plotting.average_spec('Lay_Absorb_%d' % act_lay_nu, 'Av_Absorb',  
+    len(wavelengths), 1)
+plotting.average_spec('Lay_Trans_%d'  % act_lay_nu, 'Av_Trans',   
+    len(wavelengths), 1)
+plotting.average_spec('Reflectance', 'Av_Reflec',  
+    len(wavelengths), 1)
+# Interpolate solar spectrum and calculate efficiency
+Efficiency = plotting.irradiance('Av_Absorb', 'Weighted_Absorb', 'Av_Trans', 'Weighted_Trans',
+ 'Av_Reflec', 'Weighted_Reflec', lay_print_params.radius1, period, ff = lay_print_params.ff)
+# Plot averaged sprectra
+last_light_object = light_list.pop()
+spec_list = ['Av_Absorb', 'Av_Trans', 'Av_Reflec']
+plotting.tra_plot('Spectra', spec_list, lay_print_params, last_light_object,
+    max_num_BMs, simo_para.max_order_PWs, Efficiency)
+# Plot weighted averaged sprectra
+spec_list = ['Weighted_Absorb', 'Weighted_Trans', 'Weighted_Reflec']
+plotting.tra_plot('Spectra_weighted', spec_list, lay_print_params, last_light_object, 
+    max_num_BMs, simo_para.max_order_PWs, Efficiency)
+# # Plot dispersion diagrams for each layer
+# plotting.omega_plot(solar_cell, lay_print_params, last_light_object, 
+#     max_num_BMs, simo_para.max_order_PWs, Efficiency)
+
+
+# Wrapping up simulation by printing to screen and log file
+print '\n*******************************************'
+print 'The ultimate efficiency is %12.8f' % Efficiency
+print '-------------------------------------------'
+
+# Calculate and record the (real) time taken for simulation
+elapsed = (time.time() - start)
+hms     = str(datetime.timedelta(seconds=elapsed))
+hms_string = 'Total time for simulation was \n \
+    %(hms)s (%(elapsed)12.3f seconds)'% {
+            'hms'       : hms,
+            'elapsed'   : elapsed, }
+
+python_log = open("python_log.log", "w")
+python_log.write(hms_string)
+python_log.close()
+
+print hms_string
+print '*******************************************'
