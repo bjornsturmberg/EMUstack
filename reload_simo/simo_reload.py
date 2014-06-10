65b3d3cd499c985f81621d4eb7a04bd03538e685
Bjorn Sturmberg <bjorn.sturmberg@physics.usyd.edu.au>
2014-06-10 16:10:12 +1000

    Slight edit to reloading script.

--- reload_simo/simo_reload.py
+++ reload_simo/simo_reload.py
@@ -11,11 +11,15 @@ import materials
 import plotting
 from stack import *
 
-
-data = np.load('../fig_of_merit/Simo_results20140603173129.npz')
+directory = '/home/bjorn/Results/14_06-Ev_FoMs/fig_of_merit-900-1100/'
+npz_file  = 'Simo_results20140610060241.npz'
+data = np.load(directory+npz_file)
 stacks_list = data['stacks_list']
 
 """ stacks_list is now just like in the original simo.py and we can access/plot as before. """
 
 ### Plot transmission, reflection and absorption spectra
-plotting.t_r_a_plots(stacks_list) 
+# plotting.t_r_a_plots(stacks_list) 
+
+# plotting.evanescent_merit(stacks_list, lay_interest=0)
+# plotting.evanescent_merit(stacks_list, lay_interest=1)
