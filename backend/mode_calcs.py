1D field plotting within NanoStructs fully functional including correct scaling. About to fix scaling within 2D NanoStructs.

backend/mode_calcs.py
@@ -240,7 +240,7 @@ class Simmo(Modes):
         self.prop_consts    = None
         self.mode_pol       = None
 
-    def calc_modes(self, num_BM = None, delete_working = True):
+    def calc_modes(self, num_BM = None):
         """ Run a Fortran FEM caluculation to find the modes of a \
         structured layer. """
         st = self.structure
@@ -310,18 +310,18 @@ class Simmo(Modes):
                     struct.n_msh_el, struct.table_nod,
                     struct.type_el, struct.x_arr, itermax, FEM_debug,
                     struct.mesh_file, self.n_effs, self.k_pll_norm()[0],
-                    self.k_pll_norm()[1], shift, struct.plot_BMs,
+                    self.k_pll_norm()[1], shift, struct.plotting_fields,
                     struct.plot_real, struct.plot_imag, struct.plot_abs,
                     num_pw_per_pol, num_pw_per_pol_2d, world_1d )
 
-                self.k_z, J, J_dag, J_2d, J_dag_2d, self.sol1, self.sol2, \
-                self.sol_P2 = resm
+                self.k_z, J, J_dag, J_2d, J_dag_2d, self.sol1 = resm
 
                 if self.structure.world_1d == True:
                     self.J, self.J_dag = np.mat(J), np.mat(J_dag)
                 else:
                     self.J, self.J_dag = np.mat(J_2d), np.mat(J_dag_2d)
-                del J_2d, J_dag_2d
+                J_2d = None
+                J_dag_2d = None
 
             except KeyboardInterrupt:
                 print "\n\n1D FEM routine calc_modes_1d",\
@@ -339,49 +339,43 @@ class Simmo(Modes):
             cmplx_max = 2**27#30
 
             try:
-                resm = EMUstack.calc_2d_modes(
+                resm = EMUstack.calc_modes_2d(
                     self.wl_norm(), self.num_BM, self.max_order_PWs, FEM_debug,
                     self.structure.mesh_file, self.n_msh_pts, self.n_msh_el,
                     self.structure.nb_typ_el, self.n_effs, self.k_pll_norm(),
                     shift, self.E_H_field, i_cond, itermax,
-                    self.structure.plot_BMs, self.structure.plot_real,
+                    self.structure.plotting_fields, self.structure.plot_real,
                     self.structure.plot_imag, self.structure.plot_abs,
                     num_pw_per_pol, cmplx_max)
 
-                self.k_z, J, J_dag, self.sol1, self.sol2, self.mode_pol, \
+                self.k_z, J, J_dag, self.sol1, self.mode_pol, \
                 self.table_nod, self.type_el, self.x_arr = resm
                 self.J, self.J_dag = np.mat(J), np.mat(J_dag)
 
             except KeyboardInterrupt:
-                print "\n\n2D FEM routine calc_2d_modes",\
+                print "\n\n2D FEM routine calc_modes_2d",\
                 "interrupted by keyboard.\n\n"
 
         else:
             raise ValueError,  "NanoStruct layer must have periodicity of \
                 either '1D_array' or '2D_array'."
 
-
-        if delete_working:
-            self.sol2 = None
-
         if not self.structure.plot_field_conc:
             self.mode_pol = None
 
-        if not self.structure.plotting_fields:
-            del self.sol1
-            del self.n_effs
-            del self.E_H_field
+        if self.structure.plotting_fields != 1:
+            self.sol1 = None
+            self.n_effs = None
+            self.E_H_field = None
             if self.structure.periodicity == '2D_array':
-                del self.table_nod
-                del self.type_el
-                del self.x_arr
-                del self.n_msh_pts
-                del self.n_msh_el
-            if self.structure.periodicity == '1D_array':
-            del self.sol_P2
+                self.table_nod = None
+                self.type_el = None
+                self.x_arr = None
+                self.n_msh_pts = None
+                self.n_msh_el = None
 
         ## To do, work out how to automagically process to png
-        # if self.structure.plot_BMs:
+        # if self.structure.plotting_fields:
         #     gmsh_cmd = 'gmsh '+ 'Bloch_fields/PNG/' + '*.geo'
         #     os.system(gmsh_cmd)
 