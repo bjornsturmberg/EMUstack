e8e8802fbd3a21b598586930815e06891ff0282b
Bjorn Sturmberg <bjorn.sturmberg@physics.usyd.edu.au>
2014-07-25 10:25:51 +1000

    Added GaN, AlN refractive indices and updated Al2O3 data.

--- backend/materials.py
+++ backend/materials.py
@@ -81,6 +81,10 @@ class Material(object):
         +--------------------+------------+------------------------+
         |    Ge              |            |                        |
         +--------------------+------------+------------------------+
+        |    AlN             |            |                        |
+        +--------------------+------------+------------------------+
+        |    GaN             |            |                        |
+        +--------------------+------------+------------------------+
         |    CH3NH3PbI3      |            |                        |
         +--------------------+------------+------------------------+
 
@@ -157,7 +161,7 @@ FeS2       = Material(np.loadtxt('%sFeS2.txt'% data_location))
 Zn3P2      = Material(np.loadtxt('%sZn3P2.txt'% data_location))
 Sb2S3      = Material(np.loadtxt('%sSb2S3.txt'% data_location))
 AlGaAs     = Material(np.loadtxt('%sAlGaAs.txt'% data_location))
-Al2O3      = Material(np.loadtxt('%sAl2O3.txt'% data_location))       # http://refractiveindex.info/?group=CRYSTALS&material=Al2O3
+Al2O3      = Material(np.loadtxt('%sAl2O3.txt'% data_location))       # http://refractiveindex.info/?shelf=main&book=Al2O3&page=Malitson-o
 GaAs       = Material(np.loadtxt('%sGaAs.txt'% data_location))        # http://www.filmetrics.com/refractive-index-database/GaAs/Gallium-Arsenide
 InGaAs     = Material(np.loadtxt('%sInGaAs.txt'% data_location))      # http://refractiveindex.info/?group=CRYSTALS&material=InGaAs
 Si3N4      = Material(np.loadtxt('%sSi3N4.txt'% data_location))       # http://www.filmetrics.com/refractive-index-database/Si3N4/Silicon-Nitride-SiN
@@ -165,6 +169,8 @@ MgF2       = Material(np.loadtxt('%sMgF2.txt'% data_location))        # http://w
 InP        = Material(np.loadtxt('%sInP.txt'% data_location))
 InAs       = Material(np.loadtxt('%sInAs.txt'% data_location))        # Filmetrics.com
 GaP        = Material(np.loadtxt('%sGaP.txt'% data_location))         # Filmetrics.com
+GaN        = Material(np.loadtxt('%sGaN.txt'% data_location))         # http://www.filmetrics.com/refractive-index-database/GaN/Gallium-Nitride
+AlN        = Material(np.loadtxt('%sAlN.txt'% data_location))         # http://www.filmetrics.com/refractive-index-database/AlN/Aluminium-Nitride
 Ge         = Material(np.loadtxt('%sGe.txt'% data_location))          # http://www.filmetrics.com/refractive-index-database/Ge/Germanium
 CH3NH3PbI3 = Material(np.loadtxt('%sCH3NH3PbI3.txt'% data_location))  # doi:10.1038/nmat3911 - (noting bandedge @ 800nm) Michael Graetzel, Nature Materials
 # Metals
