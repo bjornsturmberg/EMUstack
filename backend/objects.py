084185c4e24788612ad93995a6dac92df25aaf3c
Bjorn Sturmberg <bjorn.sturmberg@physics.usyd.edu.au>
2015-08-03 14:54:54 +1000

    Reinstated complex wavelengths, with the following warning;
    "Warning: using a complex wavelength. EMUstack can
    only handle these for uniform films using 0 pw_orders."

--- backend/objects.py
+++ backend/objects.py
@@ -1073,7 +1073,12 @@ class Light(object):
     """
     def __init__(self, wl_nm, max_order_PWs=2, k_parallel=None,
                  theta=None, phi=None, n_inc=1.):
-        self.wl_nm = float(wl_nm)
+        if np.imag(wl_nm) != 0:
+            self.wl_nm = complex(wl_nm)
+            print "Warning: using a complex wavelength. EMUstack can \n\
+                only handle these for uniform films using 0 pw_orders."
+        else:
+            self.wl_nm = float(np.real(wl_nm))
         self._air_anallos = {}
         self.max_order_PWs = max_order_PWs
 
